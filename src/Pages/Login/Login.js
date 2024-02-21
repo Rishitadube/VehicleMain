49ddeb8979a62d988b086fcb8644646a8e5c8a62
Dube <risdube@apac.corpdir.net>
2024-02-21 23:52:10 +0530

    login to sign up added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Login.css"
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
@@ -7,6 +8,7 @@ const Login = () => {
         <h1>Login</h1>
         <input type='email' placeholder='Email ID'/>
         <input type='password' placeholder='Password'/>
+        <div><h6>New member? <Link to="/SignUp">Sign Up</Link></h6></div>
         <div className='login-btn'>Login</div>
     </div>
   )
