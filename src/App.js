3e53c6134b14777756df4239d81cd631e627e6dc
anagha <anagha.gh@gmail.com>
2024-02-21 23:25:23 +0530

    Added routes to pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import { Outlet, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
+    <div className=''>
     <Router>
-      <div className="App">
-        <Sidebar />
-      </div>
+      <Routes>
+        <Route path='/' element={ <Login /> } />
+        <Route path='/signup' element={ <SignUp /> } />
+        {/* pages with sidebar */}
+        <Route path='/dashboard' element={<SidebarLayout />}>
+          <Route index element={<Dashboard />} />
+          {/* Add other routes with sidebar here */}
+        </Route>
+      </Routes>
     </Router>
+    </div>
   );
+}
+
+function SidebarLayout() {
+  return (
+    <div>
+      <Sidebar />
+      <Outlet />
+    </div>
+  )
 }
 
 export default App;
