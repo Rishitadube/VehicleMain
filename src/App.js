088340c63b093267e9ef2ea3d6b4ac5f79cfa4d8
Patil <patianu@apac.corpdir.net>
2024-02-22 12:27:31 +0530

    added maintennace dates

--- src/App.js
+++ src/App.js
@@ -1,72 +1,81 @@
-import './App.css';
-import Sidebar from './Components/Sidebar';
-import Dashboard from './Pages/Dashboard';
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/SignUp/SignUp';
-import MaintenanceHistory from './Components/MaintenanceHistory';
-import MaintenanceDates from './Components/MaintenanceDates';
-import AddCars from './Pages/AddCars/AddCars';
-import { Outlet, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
+import MaintenanceHistory from "./Components/MaintenanceHistory";
+import MaintenanceDates from "./Components/MaintenanceDates";
+import AddCars from "./Pages/AddCars/AddCars";
+import {
+  Outlet,
+  Routes,
+  Route,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 function App() {
-
   const maintenanceRecords = [
     {
-      date: '2023-01-15',
-      service: 'Oil Change',
+      date: "2023-01-15",
+      service: "Oil Change",
       cost: 1000,
-      comment: 'Changed oil as part of yearly maintenance.',
+      comment: "Changed oil as part of yearly maintenance.",
     },
     {
-      date: '2023-02-28',
-      service: 'Steering',
+      date: "2023-02-28",
+      service: "Steering",
       cost: 10000,
-      comment: 'Steering wheel was cleaned and polished.',
+      comment: "Steering wheel was cleaned and polished.",
     },
     {
-      date: '2023-02-12',
-      service: 'Tire Alignment',
-      cost: 15000 ,
-      comment: 'Front tires were unaligned, hence aligned the front tires.' ,
+      date: "2023-02-12",
+      service: "Tire Alignment",
+      cost: 15000,
+      comment: "Front tires were unaligned, hence aligned the front tires.",
     },
     {
-      date: '2023-02-29',
-      service: 'Brake',
+      date: "2023-02-29",
+      service: "Brake",
       cost: 500,
-      comment: 'Checked the braking system, everything was good.' ,
+      comment: "Checked the braking system, everything was good.",
     },
-
   ];
 
   return (
-    <div className=''>
-    <Router>
-      <Routes>
-        <Route index path='/' element={ <Login /> } />
-        <Route index path='/signup' element={ <SignUp /> } />
-        {/* pages with sidebar */}
-        <Route element={<SidebarLayout />}>
-          <Route index path='/dashboard' element={<Dashboard />} />
-          <Route index path='/main-rec' element={<MaintenanceHistory records={maintenanceRecords} />} />
-          <Route index path='/mycars' element={<AddCars/>} />
-          <Route index path='/main-dates' element={<MaintenanceDates/>} />
-          {/* Add other routes with sidebar here */}
-          
-        </Route>
-
-      </Routes>
-    </Router>
+    <div className="">
+      <Router>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route index path="/signup" element={<SignUp />} />
+          {/* pages with sidebar */}
+          <Route element={<SidebarLayout />}>
+            <Route index path="/dashboard" element={<Dashboard />} />
+            <Route
+              index
+              path="/main-rec"
+              element={<MaintenanceHistory records={maintenanceRecords} />}
+            />
+            <Route
+              index
+              path="/main-dates"
+              element={<MaintenanceDates records={maintenanceRecords} />}
+            />
+            <Route index path="/mycars" element={<AddCars />} />
+            {/* Add other routes with sidebar here */}
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 function SidebarLayout() {
   return (
-    <div className='d-flex'>
+    <div className="d-flex">
       <Sidebar />
       <Outlet />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Components/MaintenanceDates.js
+++ src/Components/MaintenanceDates.js
@@ -1,57 +1,13 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { styled } from "@mui/material/styles";
-import Rating from "@mui/material/Rating";
-import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
-import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
-import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
-import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAltOutlined";
-import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
 import OilBarrelIcon from "@mui/icons-material/OilBarrel";
 import TireRepairRoundedIcon from "@mui/icons-material/TireRepairRounded";
 import brake from "../assets/brake.png";
 import steering from "../assets/steering.png";
 
-const StyledRating = styled(Rating)(({ theme }) => ({
-  "& .MuiRating-iconEmpty .MuiSvgIcon-root": {
-    color: theme.palette.action.disabled,
-  },
-}));
-
-const customIcons = {
-  1: {
-    icon: <SentimentVeryDissatisfiedIcon color="error" />,
-    label: "Very Dissatisfied",
-  },
-  2: {
-    icon: <SentimentDissatisfiedIcon color="error" />,
-    label: "Dissatisfied",
-  },
-  3: {
-    icon: <SentimentSatisfiedIcon color="warning" />,
-    label: "Neutral",
-  },
-  4: {
-    icon: <SentimentSatisfiedAltIcon color="success" />,
-    label: "Satisfied",
-  },
-  5: {
-    icon: <SentimentVerySatisfiedIcon color="success" />,
-    label: "Very Satisfied",
-  },
-};
-
-function IconContainer(props) {
-  const { value, ...other } = props;
-  return <span {...other}>{customIcons[value].icon}</span>;
-}
-
-IconContainer.propTypes = {
-  value: PropTypes.number.isRequired,
-};
 
 const MaintenanceDates = ({ records }) => {
-  const [ratings, setRatings] = useState({});
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearch = (event) => {
@@ -61,20 +17,6 @@ const MaintenanceDates = ({ records }) => {
   const filteredRecords = records.filter((record) =>
     record.service.toLowerCase().includes(searchTerm.toLowerCase())
   );
-
-  useEffect(() => {
-    // Fetch ratings from JSON database or local storage when component mounts
-    const storedRatings = JSON.parse(localStorage.getItem("ratings")) || {};
-    setRatings(storedRatings);
-  }, []);
-
-  const handleRatingChange = (index, value) => {
-    // Update rating for a particular record
-    const newRatings = { ...ratings, [index]: value };
-    setRatings(newRatings);
-
-    localStorage.setItem("ratings", JSON.stringify(newRatings));
-  };
 
   const serviceIcons = {
     "Oil Change": <OilBarrelIcon />,
@@ -87,7 +29,7 @@ const MaintenanceDates = ({ records }) => {
     <div className="container-fluid bg-black py-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <h2 className="text-white mb-4">Maintenance History</h2>
+          <h2 className="text-white mb-4">Maintenance Due</h2>
           <input
             type="text"
             className="form-control mb-4"
@@ -109,18 +51,6 @@ const MaintenanceDates = ({ records }) => {
                   <strong>Cost:</strong> Rs.{record.cost} <br />
                   <strong>Comment:</strong> {record.comment} <br />
                 </p>
-                <div className="star-rating">
-                  <StyledRating
-                    name={`rating-${index}`}
-                    value={ratings[index] || 0}
-                    IconContainerComponent={IconContainer}
-                    getLabelText={(value) => customIcons[value].label}
-                    onChange={(event, newValue) =>
-                      handleRatingChange(index, newValue)
-                    }
-                    highlightSelectedOnly
-                  />
-                </div>
               </div>
             </div>
           ))}
